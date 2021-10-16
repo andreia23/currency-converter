3477370bd95b008f52d99b007bb0dfb34bd9c905
andreia23 <andreialima.berto08@gmail.com>
2021-10-16 20:49:00 -0300

    feat: queries to search user by id and transactions by user id

--- src/main/java/com/jaya/challenge/api/currency/converter/repository/TransactionRepository.java
+++ src/main/java/com/jaya/challenge/api/currency/converter/repository/TransactionRepository.java
@@ -1,13 +1,21 @@
 package com.jaya.challenge.api.currency.converter.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jaya.challenge.api.currency.converter.model.entity.Transaction;
+import com.jaya.challenge.api.currency.converter.model.entity.User;
 
 /**
  * @author andreia
  *
  */
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>  {
+	
+	@Query("select t from Transaction t where t.idUser = ?1")
+	List<Transaction> transactionsByUser(Optional<User> user);
 
 }
--- src/main/java/com/jaya/challenge/api/currency/converter/repository/UserRepository.java
+++ src/main/java/com/jaya/challenge/api/currency/converter/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.jaya.challenge.api.currency.converter.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.jaya.challenge.api.currency.converter.model.entity.User;
 
@@ -8,6 +12,11 @@ import com.jaya.challenge.api.currency.converter.model.entity.User;
  * @author andreia
  *
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	@Query("select u from User u where u.idUser = ?1")
+	Optional<User> findById(Integer idUser);
+
 
 }
