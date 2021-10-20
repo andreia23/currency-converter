ee23835d4a6d8ed38fb7bbbca2a067ff5f220fbc
andreia23 <andreialima.berto08@gmail.com>
2021-10-20 10:47:19 -0300

    fix: removing optional and ModelMapperr

--- src/main/java/com/jaya/challenge/api/currency/converter/service/TransactionService.java
+++ src/main/java/com/jaya/challenge/api/currency/converter/service/TransactionService.java
@@ -1,41 +1,40 @@
 package com.jaya.challenge.api.currency.converter.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.jaya.challenge.api.currency.converter.exception.UserNotFoundException;
 import com.jaya.challenge.api.currency.converter.model.entity.Transaction;
 import com.jaya.challenge.api.currency.converter.model.entity.User;
 import com.jaya.challenge.api.currency.converter.repository.TransactionRepository;
 import com.jaya.challenge.api.currency.converter.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author andreia
- *
  */
 @Service
 public class TransactionService {
 
-	private TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepository;
 
-	private UserRepository userRepository;
+    private UserRepository userRepository;
 
-	@Autowired
-	public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository) {
-		this.transactionRepository = transactionRepository;
-		this.userRepository = userRepository;
-	}
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository) {
+        this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
+    }
 
-	public List<Transaction> findALL() {
-		return transactionRepository.findAll();
-	}
+    public List<Transaction> findALL() {
+        return transactionRepository.findAll();
+    }
 
-	public List<Transaction> transactionsByUser(Integer idUser) {
-
-		Optional<User> user = userRepository.findById(idUser);
-		return transactionRepository.transactionsByUser(user);
-	}
+    public List<Transaction> transactionsByUser(Long idUser) {
+        User user = userRepository.findById(idUser).orElseThrow(
+                () -> new UserNotFoundException("User not found"));
+        ;
+        return transactionRepository.transactionsByUser(user);
+    }
 
 }
--- src/main/java/com/jaya/challenge/api/currency/converter/service/UserService.java
+++ src/main/java/com/jaya/challenge/api/currency/converter/service/UserService.java
@@ -1,42 +1,31 @@
 package com.jaya.challenge.api.currency.converter.service;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
+import com.jaya.challenge.api.currency.converter.model.dto.UserDTO;
+import com.jaya.challenge.api.currency.converter.model.entity.User;
+import com.jaya.challenge.api.currency.converter.model.request.UserRequest;
+import com.jaya.challenge.api.currency.converter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.jaya.challenge.api.currency.converter.model.dto.UserDTO;
-import com.jaya.challenge.api.currency.converter.model.entity.User;
-import com.jaya.challenge.api.currency.converter.repository.UserRepository;
-
 /**
  * @author andreia
- *
  */
 @Service
 public class UserService {
 
-	private UserRepository userRepository;
+    private UserRepository userRepository;
 
-	@Autowired
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-	@Transactional
-	public UserDTO saveUser(UserDTO userDTO) {
-
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-		User userEntity = modelMapper.map(userDTO, User.class);
-
-		userRepository.save(userEntity);
-
-		UserDTO returnValue = modelMapper.map(userEntity, UserDTO.class);
-
-		return returnValue;
-	}
+    @Transactional
+    public UserDTO saveUser(UserRequest userRequest) {
+        User userEntity = userRequest.toModel();
+        userEntity = userRepository.save(userEntity);
+        return new UserDTO(userEntity);
+    }
 
 }
